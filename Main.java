b1adfb14e4f7d3faaf254067c5d58d070294a358
Pierpaolo Rolli <pierolli1996@gmail.com>
2026-02-07 09:26:43 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+  
+Persona a=new Studente("Pierpaolo",29,"23242");
+Persona b=new Lavoratore("Pierpaolo",24,1200);
+
+a.descrizione();
+b.descrizione();
+    
+    }
+    
+}
